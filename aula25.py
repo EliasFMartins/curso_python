26ac3a2bcf094caa1c7d5bd587a80d1228e51316
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-25 20:47:06 -0300

    feat: exercicio 25att

--- aula25.py
+++ aula25.py
@@ -1,4 +1,19 @@
 # Operadores in e not in
-# String sao interaveis
-# 0 1 2 3 4 5
-# E l i a s
+# String sao iteraveis
+#  0 1 2 3 4 =>
+#  E l i a s
+# -5-4-3-2-1
+
+nome = 'Elias'
+print(nome[2])
+print(10*'----')
+print('a' in nome)
+# mostra se tem o caracter na variavel o not in faz o inverso se tiver fala que nao e assim por diante
+
+
+nome2 = input('digite seu nome :')
+encontrar = input('Digite o que deseja encontrar: ')
+if encontrar in nome2:
+  print(f'{encontrar} esta em {nome2}')
+else:
+  print(f' ta aqui nao fera fods')
