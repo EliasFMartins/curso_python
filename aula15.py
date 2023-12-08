bf642260ef1c1e53960af3330cf26f8654ec72ca
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-08 14:37:44 -0300

    add anotacoes as aulas  e revizando

---
+++ aula15.py
@@ -0,0 +1,11 @@
+# if/ elif       / else
+entrada = input('Voce quer entrar ou sair ? ')
+
+if entrada == 'entrar':
+	print('voce entrou no sistema')
+elif entrada =='sair':	
+	print('voce saiu do sistema')
+else:
+	print('nenhum baguio foi baguiado')
+
+    
