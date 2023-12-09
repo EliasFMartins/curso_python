3c016a0d6f1c8cb9517c9982f6bcab3f3fc40ab7
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-09 15:58:08 -0300

    add anotacoes as aulas  e revizando

--- aula27.py
+++ aula27.py
@@ -1,3 +1,12 @@
+# .<numero de digitos> f
+# (Caractere) (><^)(quantidade)
+# > - esquerda
+# < - direita
 variavel =  'ABC'
 print(f'{variavel}')
 print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel:%^10}.')
+print(f'{1000.4873648123746:0>+10,.1f}.')
+print(f'o hexadecimal de 1500 e {1500:08X}')
+print(f'o hexadecimal de 1500 e {1500:X}')
