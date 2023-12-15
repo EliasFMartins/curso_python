d154e5a36f47fb04a82a0bf47986644ace21e659
Elias F Martins <elias.martins@defensoria.df.gov.br>
2023-12-15 10:00:39 -0300

    introdução ao desempacotamento

---
+++ aula49.py
@@ -0,0 +1,10 @@
+"""
+Introdução ao desempacotamento  
+"""
+# nomes  = ['Elias', 'Gabriel GG', 'Fasael']
+# nome1, nome2, nome3 = nomes
+_,name, *_ = ['Raimunda', 'Fransisnalda', 'Bernadeth']
+#_ e uma convercao q e usada pra salvar valores que nao seram utilizados 
+# print(nomes1)
+print(name)
+print(_)
