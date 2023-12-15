fc17f430cb9da3f8275c022c716eee755f0270ad
Elias F Martins <elias.martins@defensoria.df.gov.br>
2023-12-15 12:10:29 -0300

    introdução ao enumerate

---
+++ aula52.py
@@ -0,0 +1,5 @@
+"""
+Faça uma lista de comprar com listas o usuário deve ter a possibilidade de inserir, apagar e listar valores da sua lista não permita que
+o programa quebre com erros de índices inexistentes na lista 
+
+"""
