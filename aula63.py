4e4c0858f9cab69330dc4ca7f98997742558691f
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-16 17:57:13 -0300

    funções em python (def)

--- aula63.py
+++ aula63.py
@@ -5,6 +5,7 @@ replicar determinada ação ao longo do seu código.
 Elas podem receber valores para parâmetros (argumentos)
 e retornar um valor específico.
 por padrão, funções Python retornam None (nada)
+Funções podem usar parâmetros para receber valores. Parâmetro é o nome da "variável" dentro dos parênteses, argumento é o valor passado para o parâmetro no momento da execução da função.
 """
 def Print(param ='teste'):
     print(param)
