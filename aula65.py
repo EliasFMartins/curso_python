7a90f4a86c0df0d5fcc6665694601425b3a54c1d
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-19 19:08:51 -0300

    functions with python3  and return

---
+++ aula65.py
@@ -0,0 +1,12 @@
+"""
+Retorno de valores das funções (return)
+"""
+def soma (x, y):
+    print(x +y)
+    return x + y
+    
+    
+#variavel = soma(1,2)
+#variavel = int('21')
+soma1 = soma(2,2)
+soma1 = soma(3,2)
