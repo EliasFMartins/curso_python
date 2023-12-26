68db9e8d7039b8703758660f6722e22a83b7cc49
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-26 18:39:51 -0300

    mapeamento com lista e dicionarios python

--- aula84.py
+++ aula84.py
@@ -7,6 +7,19 @@ for numero in range(10):
     lista.append(numero)
 # print(lista)
 lista = [numero for numero in range(10)]
-print(lista)  
+# print(lista)  
 lista = [numero * 2 for numero in range(10)]
-print(lista)  
+# print(lista)  
+produtos = [
+    {'nome':'p1','preco':20,},
+    {'nome':'p2','preco':30,},
+    {'nome':'p3','preco':40,},
+    {'nome':'p4','preco':50,},
+]
+novos_produtos = [
+    # {'nome': produto['nome'],'preco': produto['preco']*2} , #copia as chaves e faz alteracões
+    {**produto, 'preco':produto['preco']*2}
+    if produto['preco'] > 20 else {**produto}# copia tudo e rescreve um valor em especifico
+    for produto in produtos
+]
+print(*novos_produtos, sep='\n')
