8fc774ea9134b6cca4f36e2fe86babe6c17293bb
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-27 13:22:46 -0300

    generator in python

--- aula90.py
+++ aula90.py
@@ -1,6 +1,12 @@
+
 # Generator expression, Iterables e Iterators em Python
+import sys 
+
 iterable =['Eu', 'tenho', '__iter__']
 iterator = iter(iterable)    #.__iter__() # tem __iter__ e __next__
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+lista = [n for n in range(10)]# executa o codigo e gera a lista de numerod dentro do range toda dentro da memoria
+generator= (n for n in range(10)) # o valor fica marcado pra chamar o proximo quando vc chama apenas o valor n fica na memoria n tem index nem len ne nada 
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
+print((lista))
+print(next(generator))
