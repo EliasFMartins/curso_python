9adbfbf7774bd8296d91fb6f5fcf47f9ee1ead09
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-27 17:44:04 -0300

    Tratamento de erros try except caputarando msg de erro and name

--- aula93.py
+++ aula93.py
@@ -4,14 +4,14 @@ try:
     a = 18
     print('Linha 01')
     b = 0
-    print(b[0])
+    print('linha1'[1000])
     c = a / b
     print('Linha 02')
 
 except ZeroDivisionError: 
     print('Dividiu por 0')
-except( NameError, IndexError): #tratar mais de um erro de uma vez
-    print('Name error')
+except( NameError, IndexError) as error: #tratar mais de um erro de uma vez
+    print('Name ',error.__class__.__name__)
 except Exception:
     print('Fodeu a poha toda ')
 print('Continuar')
