8545699f6bfde6a69bace33f10122080aacc42ca
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-27 11:07:57 -0300

    filter com python

--- aula84.py
+++ aula84.py
@@ -2,6 +2,9 @@
 # List comprehesion é uma forma rápida para criar listas
 # a partir de iteráveis.
 # print(list(range(10)))
+import pprint
+def p (v):
+    pprint.pprint(v,sort_dicts=False, width=40)
 lista = []
 for numero in range(10):
     lista.append(numero)
@@ -22,4 +25,16 @@ novos_produtos = [
     if produto['preco'] > 20 else {**produto}# copia tudo e rescreve um valor em especifico
     for produto in produtos
 ]
-print(*novos_produtos, sep='\n')
+# print(*novos_produtos, sep='\n')
+lista = [n for n in range(10) if n < 5]
+print(lista)
+# oque vem a esquerda do for e mapeamento oque vem a direita e filtro
+
+novos_produtos = [
+    # {'nome': produto['nome'],'preco': produto['preco']*2} , #copia as chaves e faz alteracões
+    {**produto, 'preco':produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}# copia tudo e rescreve um valor em especifico
+    for produto in produtos
+    if produto['preco'] > 20 # sempre pega o valor do antigo nao do atualizado
+]
+p(novos_produtos)
