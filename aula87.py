ee8ab8386af2b6d2468c4ae02d954a6319fc95a3
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-27 11:51:56 -0300

    isinstace e seus usos

---
+++ aula87.py
@@ -0,0 +1,20 @@
+# isinstace -  para saber se o objeto é de determinado tipo
+lista = ['a', 1, 1.1, True, [0, 1, 2], (1, 2),
+         {0, 1}, {'nome': 'Elias'}]
+for item in lista:
+    # verifica o tipo do item e igual a set  caso seja ele passa se n ...
+    if isinstance(item, set):
+        print('SET')
+        item.add(5)
+        print(item, isinstance(item, set))
+
+    elif isinstance(item, str):
+        print('STR')
+        print(item.upper())
+
+    elif isinstance(item, (int, float)):
+        print('NUM')
+        print(item, item * 2)
+    else:
+        print('Outros')
+        print(item)
