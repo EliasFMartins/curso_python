392362b710c81f6db13118698c484798dc2ad1a2
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-27 13:12:48 -0300

    generator in python

---
+++ aula90.py
@@ -0,0 +1,6 @@
+# Generator expression, Iterables e Iterators em Python
+iterable =['Eu', 'tenho', '__iter__']
+iterator = iter(iterable)    #.__iter__() # tem __iter__ e __next__
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
