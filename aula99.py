19388fb97a6332133a3d51313cee1f5e15cb03ea
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-28 12:43:08 -0300

    package and module

---
+++ aula99.py
@@ -0,0 +1,14 @@
+from sys import path 
+from aula99_package.modulo import soma_do_modulo
+import aula99_package.modulo
+from aula99_package.modulo import *# importar tudo
+from aula99_package import modulo
+
+# print(*path, sep='\n')# diretorios que esse modulo tem acesso
+
+# import aula99_package
+
+print(soma_do_modulo(2,5))
+print(aula99_package.modulo.soma_do_modulo(2,5))
+print(modulo.soma_do_modulo(2,5))
+print(variavel)
---
+++ aula99_package/modulo.py
@@ -0,0 +1,6 @@
+__all__ = [
+    'variavel',
+]
+variavel = 'Elias'
+def soma_do_modulo(x, y):
+    return x+ y
