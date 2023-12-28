1eb2b0e364d4c95e1b03f6940cc984ded76a1ac1
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-28 11:16:56 -0300

    modulos em python padroes de importações

--- aula96.py
+++ aula96.py
@@ -3,7 +3,15 @@
 # Vantagens: você tem o nomespace do módulo
 # Desvatagens: nomes grandes
 import sys
-print(sys.platform)
+print(sys.platform)# fa;a o nome do sistema operacional
+#importa o pacote inteiro e seleciona qual funcionalidade utilizar
+from sys import exit, platform
+print(platform)
+import sys as s # altera o nome  do pacote  quando vc faz a importação
+from sys import platform as pf, exit as ex # alterando o nome da modulo do pacote na importação
+from sys import *# basicamente importa tudo  e fica all sem a proteção do sys. vc consegue consumir diretamente
+
+# seleciona oque vc quer do pacote e importa apenas eles vc pode utilizar diretamente
 
 # partes - from nome_modulo import objeto1, objeto2
 # Vantagens: nomes pequenos
