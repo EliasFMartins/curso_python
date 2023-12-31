8be4d1af4b5e519d3ccb26599e6d0beeeb72473c
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-12-30 21:57:26 -0300

    ambiente virtual em python

---
+++ aula117.py
@@ -0,0 +1,7 @@
+import pymysql
+# criar requirements.txt with python
+#gerar ambiente virtual python -m venv "nome do ambiente"por padrao e venv/ .venv /.env
+#pip freeze > requirements.txt  e o nome do arquivo gerar arquivo tipo json config no node.js
+# instalar all requeriments.txt
+#pip install -r requeriments.txt
+print('Oiee')
