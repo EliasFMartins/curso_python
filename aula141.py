53cb96c2bfd1e0bc79c558726fa86b2e87dc0d86
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-01 17:48:48 -0300

    heranca with python 2

--- aula141.py
+++ aula141.py
@@ -8,3 +8,28 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
+
+class Pessoa:
+    cpf = 1234
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+    def falar_nome_classe(self):
+        print('Nao sai da class client')
+        print(self.nome, self.__class__.__name__)
+
+
+class Client(Pessoa):
+    pass
+
+
+class Aluno(Pessoa):
+    pass
+
+
+client = Client('Elias', 'Martins')
+print(client.nome)
+print(client.sobrenome)
+client.falar_nome_classe()
+print(client.cpf)
