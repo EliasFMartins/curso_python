bd4ceead2b10b3490e2896adbc506eca91263921
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-02 11:17:11 -0300

    finalizando exercicio classes

--- aula144/main.py
+++ aula144/main.py
@@ -1,9 +1,16 @@
-from log import LogFileMixin, LogPrintMixin
+# from log import LogFileMixin, LogPrintMixin
 
-lp = LogPrintMixin()
-lp.log_error('qualquer coisa')
-lp.log_sucess('que legal')
+# lp = LogPrintMixin()
+# lp.log_error('qualquer coisa')
+# lp.log_sucess('que legal')
 
-lf = LogFileMixin()
-lf.log_error('qualquer coisa1')
-lf.log_sucess('que legal')
+# lf = LogFileMixin()
+# lf.log_error('qualquer coisa1')
+# lf.log_sucess('que legal')
+
+from eletronico import Smartphone
+
+galaxy_s = Smartphone('Galaxy S')
+iphone = Smartphone('iPhone')
+galaxy_s.ligar()
+iphone.ligar()
