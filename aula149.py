ae2a261279ec36f59971c5c9bc0a9ad12673332c
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-01-03 15:35:23 -0300

    Dunder methods with python

--- aula149.py
+++ aula149.py
@@ -15,3 +15,25 @@
 # __neg__(self) - -self
 # __str__(self) - str
 # __repr__(self) - str
+
+class Ponto:
+    def __init__(self, x, y, z='String') -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self) -> str:  # so quer a string
+        return f'({self.x}, {self.y})'
+
+    def __repr__(self) -> str:  # pra dev
+        class_name = self.__class__.__name__  # mema coisa
+        class_name = type(self).__name__
+        return f'{class_name} (x={self.x}, y={self.y!r} z={self.z!r})'
+
+
+p1 = Ponto(1, 2)
+p2 = Ponto(978, 876)
+print(p1)
+print(repr(p2))
+print(f'{p2!r}')  # esse chama o __rep__
+print(f'{p2!s}')  # esse chama o __str__
